13743317ccc6814dd23dd5376f74578efdb2f4fb
kazuki1597 <144293988+kazuki1597@users.noreply.github.com>
2023-09-19 18:43:21 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,24 +1,38 @@
+const buildHTML = (XHR) => {
+    const item = XHR.response.post;
+    const html = `
+      <div class="post">
+        <div class="post-date">
+          投稿日時：${item.created_at}
+        </div>
+        <div class="post-content">
+          ${item.content}
+        </div>
+      </div>`;
+    return html;
+  };  
+
 function post (){
-    //リクエストを送信する処理
     const form = document.getElementById("form");
-    // getElementByIdメゾット　クリック対象となる投稿ボタンの要素を取得できるようになる
     form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        // preventDefault()メソッドとは、既定のイベント（投稿ボタンをクリックするなど）を無効化するメゾッド
-        // eとは、イベント発生時の情報を持ったオブジェクト
-        const formData = new FormData(form);
-        // formDataオブジェクトとは、フォームに入力された値を取得できるオブジェクトのこと
-        const XHR = new XMLHttpRequest();
-        // XMLHttpRequestオブジェクトとは、JavaScriptを用いてサーバーとHTTP通信行うときに利用するオブジェクト
-        XHR.open("POST", "/posts", true);
-        // openメゾットとは、リクエストの内容を指定するためのメソッド
-        XHR.responseType = "json";
-        // responseTypeプロパティとは、レスポンスのデータフォーマット（＝どのような形式のデータにするか）を指定するプロパティ
-        // JSONとは、JavaScriptをもとにして構成されたデータフォーマットのこと
-        XHR.send(formData);
-        // sendメソッドとはリクエストを送信するメソッド
-    }); 
-};
-   
-   window.addEventListener('turbo:load', post);
-//    addEventListener（アドイベントリスナー）とはイベント発火の際に実行するためのメソッド
+      e.preventDefault();
+      const formData = new FormData(form);
+      const XHR = new XMLHttpRequest();
+      XHR.open("POST", "/posts", true);
+      XHR.responseType = "json";
+      XHR.send(formData);
+      XHR.onload = () => {
+        if (XHR.status != 200) {
+            alert(`Error ${XHR.status}: ${XHR.statusText}`);
+            return null;
+          };
+        const list = document.getElementById("list");
+        const formText = document.getElementById("content");
+        list.insertAdjacentHTML("afterend", buildHTML(XHR));
+        formText.value = "";
+    };
+    });
+  };
+  
+  window.addEventListener('turbo:load', post);
+  
